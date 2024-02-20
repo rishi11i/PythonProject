dd1febf5950c7f9438dffaf5d3d6f4a3dd4ca3ba
RISHIKESH SINGH BHARDWAJ <160471229+rishi11i@users.noreply.github.com>
2024-02-20 20:47:11 +0530

    Create Calculator.py
    Here You can calculate anything

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter 1st Number : "))
+num2=int(input("Enter 2nd Number : "))
+op=input("Enter oprator sign : ")[0]
+if op=="+":
+    print("Addition of", num1," $", num2," = ",num1+num2)
+
+elif op=="-":
+    print("Sub of ",num1," $ ",num2," = ",num1-num2)
+
+elif op=="*":
+    print("miltipy of ",num1," $ ",num2," = ",num1*num2 )
+elif op=="/":
+    print("divide of ",num1," $ ",num2," =" ,num1/num2)
